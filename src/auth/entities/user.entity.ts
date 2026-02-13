0827e0d7d7a676fd6dcc9b26bf45014fd261baec
Charly <jchar7879@hotmail.com>
2026-02-13 10:42:56 -0300

    Entidad user

---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,39 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({
+        type: 'varchar',
+        length: 250,
+        unique: true,
+    })
+    email: string;
+
+    @Column({
+        type: 'text',
+    })
+    password: string;
+
+    @Column({
+        type: 'varchar',
+        length: 250,
+    })
+    fullName: string;
+
+    @Column({
+        type: 'bool',
+    })
+    isActive: boolean;
+
+    @Column({
+        type: 'text',
+        array: true,
+        default: ['user'],
+    })
+    roles: string[];
+
+}
