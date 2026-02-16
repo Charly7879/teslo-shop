a20f6965237e7c0c74073cace678f04fbc91cda8
Charly <jchar7879@hotmail.com>
2026-02-16 11:34:13 -0300

    jwtStrategy 02

---
+++ src/auth/interfaces/jwt-payload.interface.ts
@@ -0,0 +1,4 @@
+export interface JwtPayload {
+    email: string;
+    //TODO: Añadir lo que se quiera grabar
+}
---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,48 @@
+/**
+ * JwtStratey (Provider)
+ * Estragetia para configurar JWT
+ */
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { User } from "../entities/user.entity";
+import { JwtPayload } from "../interfaces/jwt-payload.interface";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { ConfigService } from '@nestjs/config';
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+
+    constructor(
+        @InjectRepository(User)
+        private readonly userRepository: Repository<User>,
+        configService: ConfigService,
+    ) {
+        super({
+            secretOrKey: configService.get('JWT_SECRET') as string,
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+        });
+    }
+
+    /**
+     * Se llama sí el JWT no ha expirado 
+     * y sí la firma jwt coincide con el payload
+     * @param payload 
+     */
+    async validate(payload: JwtPayload): Promise<User> {
+
+        const { email } = payload;
+
+        const user = await this.userRepository.findOneBy({ email });
+
+        if (!user)
+            throw new UnauthorizedException(`Token not valid`);
+
+        if (!user.isActive)
+            throw new UnauthorizedException(`User is inactive`);
+
+        return user;
+    }
+
+}
