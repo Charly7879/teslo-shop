aae6805377bf0d33e407ae83e1629be8154b6b0a
Charly <jchar7879@hotmail.com>
2026-02-17 14:28:12 -0300

    Verificar rol del usuario

--- src/auth/guards/user-role.guard.ts
+++ src/auth/guards/user-role.guard.ts
@@ -2,9 +2,10 @@
  * UserRoleGuard:
  * Custom Guard encargado de ver el usuario y sí tienes los roles para acceder a un route.
  */
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { BadRequestException, CanActivate, ExecutionContext, ForbiddenException, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
+import { User } from '../entities/user.entity';
 
 @Injectable()
 export class UserRoleGuard implements CanActivate {
@@ -18,13 +19,33 @@ export class UserRoleGuard implements CanActivate {
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
 
-    console.log('UserRoleGuard');
-
     // Data del decorator SetMetadata()
     const validRoels: string[] = this.reflector.get('roles', context.getHandler());
 
-    console.log(validRoels)
+    // Comprobar sí está SetMetaData
+    if (!validRoels) return true;
 
-    return true;
+    if (validRoels.length === 0) return true;
+
+    // Request
+    const req = context.switchToHttp().getRequest();
+
+    // User
+    const user: User = req.user;
+
+    //console.log(validRoels)
+
+    if (!user)
+      throw new BadRequestException('User not found');
+
+    //console.log({ userRoles: user.roles })
+
+    // Validar user por los roles
+    for (const role of user.roles) {
+      if (validRoels.includes(role))
+        return true;
+    }
+
+    throw new ForbiddenException(`User ${user.fullName} need valid roles`);
   }
 }
