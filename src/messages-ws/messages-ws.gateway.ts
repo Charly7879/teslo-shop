1cecc6b94bee6abe944a47493866850c67e0df2f
Charly <jchar7879@hotmail.com>
2026-03-06 17:38:55 -0300

    Cliente - Detectar conexiones y desconexiones

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -21,15 +21,14 @@ export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconne
   handleConnection(client: Socket) {
     //console.log(`Cliente conectado: ${client.id}`);
     this.messageWsService.registerClient(client);
-
-    console.log(`Conectados: ${this.messageWsService.getConnectedClients()}`);
+    //console.log(`Conectados: ${this.messageWsService.getConnectedClients()}`);
   }
 
   // Called automatically when a client disconnects
   handleDisconnect(client: Socket) {
     //console.log(`Cliente desconectado: ${client.id}`);
     this.messageWsService.removeClient(client.id);
-    console.log(`Conectados: ${this.messageWsService.getConnectedClients()}`);
+    //console.log(`Conectados: ${this.messageWsService.getConnectedClients()}`);
   }
 
 }
